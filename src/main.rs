f8b2bb99116d88e44ba169d833202487f602cb00
Teapane <tyler.stephen.long@gmail.com>
2021-01-11 07:52:10 -0700

    trying out some more

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
+fn foo_bar() {
+    println!("Another function?");
+}
+
 fn main() {
     let stdout = stdout();
-    let message = String::from("Hello Rust, are you compiling?");
+    let message = String::from("Hello Rust, are you compiling? Does this change the width? Reader: Yes, it does");
     let width = message.chars().count();
+    println!("{}", width);
+
+    foo_bar();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
